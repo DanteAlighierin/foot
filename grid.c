379f7cf646c9543868ae1262fd3cf9e79f537c9a
Daniel Eklöf <daniel@ekloef.se>
2019-07-10 19:17:53 +0200

    grid: swap_row: drop requirement that row indices be positive

--- grid.c
+++ grid.c
@@ -12,11 +12,12 @@ grid_swap_row(struct grid *grid, int row_a, int row_b)
 {
     assert(grid->offset >= 0);
     assert(row_a != row_b);
-    assert(row_a >= 0);
-    assert(row_b >= 0);
 
     int real_a = (grid->offset + row_a + grid->num_rows) % grid->num_rows;
     int real_b = (grid->offset + row_b + grid->num_rows) % grid->num_rows;
+
+    assert(real_a >= 0);
+    assert(real_b >= 0);
 
     struct row *tmp = grid->rows[real_a];
     grid->rows[real_a] = grid->rows[real_b];
