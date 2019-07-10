1d338f8477d64502c71d06e2ee994084f5935054
Daniel Eklöf <daniel@ekloef.se>
2019-07-10 09:29:36 +0200

    scrollback: don't scroll past scrollback history

--- commands.c
+++ commands.c
@@ -8,6 +8,7 @@
 #include "grid.h"
 
 #define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
 
 void
 cmd_scrollback_up(struct terminal *term, int rows)
@@ -15,16 +16,29 @@ cmd_scrollback_up(struct terminal *term, int rows)
     if (term->grid == &term->alt)
         return;
 
-    assert(term->grid->offset >= 0);
-    int new_view = (term->grid->view + term->grid->num_rows - rows) % term->grid->num_rows;
+    rows = min(rows, term->grid->num_rows - term->rows);
 
-    LOG_WARN("%d" ,new_view);
+    assert(term->grid->offset >= 0);
+    assert(rows <= term->rows);
+
+    int new_view = (term->grid->view + term->grid->num_rows - rows) % term->grid->num_rows;
     assert(new_view >= 0);
     assert(new_view < term->grid->num_rows);
 
     /* Avoid scrolling in uninitialized rows */
     while (!term->grid->rows[new_view]->initialized)
         new_view = (new_view + 1) % term->grid->num_rows;
+
+    /* Don't scroll past scrollback history */
+    int end = (term->grid->offset + term->rows) % term->grid->num_rows;
+    if (end >= term->grid->offset) {
+        /* Not wrapped */
+        if (new_view >= term->grid->offset && new_view < end)
+            new_view = end;
+    } else {
+        if (new_view >= term->grid->offset || new_view < end)
+            new_view = end;
+    }
 
     LOG_DBG("scrollback UP: %d -> %d (offset = %d, rows = %d)",
             term->grid->view, new_view, term->grid->offset, term->grid->num_rows);
@@ -43,10 +57,11 @@ cmd_scrollback_down(struct terminal *term, int rows)
     if (term->grid == &term->alt)
         return;
 
-    assert(term->grid->offset >= 0);
-    int new_view = (term->grid->view + rows) % term->grid->num_rows;
+    rows = min(rows, term->grid->num_rows - term->rows);
 
-    LOG_WARN("%d" ,new_view);
+    assert(term->grid->offset >= 0);
+
+    int new_view = (term->grid->view + rows) % term->grid->num_rows;
     assert(new_view >= 0);
     assert(new_view < term->grid->num_rows);
 
@@ -64,6 +79,18 @@ cmd_scrollback_down(struct terminal *term, int rows)
             }
         }
     } while (!all_initialized);
+
+    /* Don't scroll past scrollback history */
+    int end = (term->grid->offset + term->rows) % term->grid->num_rows;
+    if (end >= term->grid->offset) {
+        /* Not wrapped */
+        if (new_view >= term->grid->offset && new_view < end)
+            new_view = term->grid->offset;
+    } else {
+        if (new_view >= term->grid->offset || new_view < end)
+            new_view = term->grid->offset;
+    }
+
 
     LOG_DBG("scrollback DOWN: %d -> %d (offset = %d, rows = %d)",
             term->grid->view, new_view, term->grid->offset, term->grid->num_rows);
