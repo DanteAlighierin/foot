9df691e1aff80249209dc9b4918eb94310d5fb3e
Daniel Eklöf <daniel@ekloef.se>
2019-07-10 16:03:18 +0200

    vt: add vt_param_get() - returns the specified parameter's value

--- vt.h
+++ vt.h
@@ -6,3 +6,14 @@
 #include "terminal.h"
 
 void vt_from_slave(struct terminal *term, const uint8_t *data, size_t len);
+
+static inline int
+vt_param_get(const struct terminal *term, size_t idx, int default_value)
+{
+    if (term->vt.params.idx > idx) {
+        int value = term->vt.params.v[idx].value;
+        return value != 0 ? value : default_value;
+    }
+
+    return default_value;
+}
