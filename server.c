cd382a467ce872f3413304d8ecc6f050cfe8a4cf
Daniel Eklöf <daniel@ekloef.se>
2019-12-21 20:05:00 +0100

    server: move forward the indicated number of TERM bytes

--- server.c
+++ server.c
@@ -185,7 +185,7 @@ fdm_client(struct fdm *fdm, int fd, int events, void *data)
     uint16_t term_env_len = *(uint16_t *)p; p += sizeof(term_env_len);
 
     CHECK_BUF(term_env_len);
-    const char *term_env = (const char *)p; p += strlen(term_env) + 1;
+    const char *term_env = (const char *)p; p += term_env_len;
     LOG_DBG("TERM = %.*s", term_env_len, term_env);
 
     if (term_env_len != strlen(term_env) + 1) {
