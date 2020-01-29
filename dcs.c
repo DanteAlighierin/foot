26d39fe96e78f835a8c8cee39e2a6f44699c4838
Daniel Eklöf <daniel@ekloef.se>
2020-01-29 19:43:58 +0100

    dcs: fix comment: ESU is *End* synchronized update

--- dcs.c
+++ dcs.c
@@ -21,7 +21,7 @@ esu(struct terminal *term)
 {
     /* https://gitlab.com/gnachman/iterm2/-/wikis/synchronized-updates-spec */
 
-    LOG_DBG("untested: ESU - Begin Synchronized Update (params: %.*s)",
+    LOG_DBG("untested: ESU - End Synchronized Update (params: %.*s)",
             (int)term->vt.dcs.idx, term->vt.dcs.data);
 
     term_disable_app_sync_updates(term);
