729a57d3a89b30ecaf3210532f798a3107fd3242
Daniel Eklöf <daniel@ekloef.se>
2020-05-03 12:40:40 +0200

    util: thrd_err_as_string: fix alignment

--- util.h
+++ util.h
@@ -10,7 +10,7 @@ static inline const char *
 thrd_err_as_string(int thrd_err)
 {
     switch (thrd_err) {
-    case thrd_success:   return "success";
+    case thrd_success:  return "success";
     case thrd_busy:     return "busy";
     case thrd_nomem:    return "no memory";
     case thrd_timedout: return "timedout";
