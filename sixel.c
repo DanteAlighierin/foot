aa3985a298d7770ef780324adea3ba0fe75b4e09
Daniel Eklöf <daniel@ekloef.se>
2020-08-23 09:37:51 +0200

    Don't use "case X ... Y:", if possible/where it makes sense

--- sixel.c
+++ sixel.c
@@ -668,7 +668,16 @@ decsixel(struct terminal *term, uint8_t c)
         term->sixel.pos.col = 0;
         break;
 
-    case '?'...'~':
+    case '?': case '@': case 'A': case 'B': case 'C': case 'D': case 'E':
+    case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L':
+    case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S':
+    case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+    case '[': case '\\': case ']': case '^': case '_': case '`': case 'a':
+    case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h':
+    case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o':
+    case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v':
+    case 'w': case 'x': case 'y': case 'z': case '{': case '|': case '}':
+    case '~':
         sixel_add(term, term->sixel.palette[term->sixel.color_idx], c - 63);
         break;
 
@@ -687,7 +696,8 @@ static void
 decgra(struct terminal *term, uint8_t c)
 {
     switch (c) {
-    case '0'...'9':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
         term->sixel.param *= 10;
         term->sixel.param += c - '0';
         break;
@@ -732,7 +742,8 @@ static void
 decgri(struct terminal *term, uint8_t c)
 {
     switch (c) {
-    case '0'...'9':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
         term->sixel.param *= 10;
         term->sixel.param += c - '0';
         break;
@@ -750,7 +761,8 @@ static void
 decgci(struct terminal *term, uint8_t c)
 {
     switch (c) {
-    case '0'...'9':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
         term->sixel.param *= 10;
         term->sixel.param += c - '0';
         break;
