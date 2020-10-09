0fa4a189384cef057d46f83296041e2220b7bcfe
Daniel Eklöf <daniel@ekloef.se>
2020-10-09 18:52:05 +0200

    term: remove unused bit in xtsave struct

--- terminal.h
+++ terminal.h
@@ -258,7 +258,6 @@ struct terminal {
         uint32_t show_cursor:1;
         uint32_t auto_margin:1;
         uint32_t cursor_blink:1;
-        uint32_t insert_mode:1;
         uint32_t bracketed_paste:1;
         uint32_t focus_events:1;
         uint32_t alt_scrolling:1;
