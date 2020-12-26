3c6789fb8ba87810a2aa83eebf6eea10c969db6d
Daniel Eklöf <daniel@ekloef.se>
2020-12-26 01:26:54 +0100

    reaper: add optional callback
    When calling ‘reaper_add()’, the caller can provide a callback. If
    non-NULL, the reaper will call the callback to handle the actual
    reaping.
    
    If the callback is NULL, or if it returns false, the reaper reaps the
    child process.

--- reaper.h
+++ reaper.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdbool.h>
 #include <sys/wait.h>
 
 #include "fdm.h"
@@ -9,4 +10,6 @@ struct reaper;
 struct reaper *reaper_init(struct fdm *fdm);
 void reaper_destroy(struct reaper *reaper);
 
-void reaper_add(struct reaper *reaper, pid_t pid);
+typedef bool (*reaper_cb)(struct reaper *reaper, pid_t pid, void *data);
+
+void reaper_add(struct reaper *reaper, pid_t pid, reaper_cb cb, void *cb_data);
