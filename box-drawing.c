00d0e53a9507df770bae46580f170b6d82cca9e3
Daniel Eklöf <daniel@ekloef.se>
2020-12-29 11:39:10 +0100

    box-drawing: oops - remove accidental ‘x’

--- box-drawing.c
+++ box-drawing.c
@@ -1508,7 +1508,7 @@ draw_glyph(wchar_t wc, uint8_t *buf, int width, int height, int stride, int dpi)
     }
 }
 
-struct fcft_glyph * COLDx
+struct fcft_glyph * COLD
 box_drawing(const struct terminal *term, wchar_t wc)
 {
     int width = term->cell_width;
