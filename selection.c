49d6dbd761af1dded9e78076dd1a6049c68f82a0
Daniel Eklöf <daniel@ekloef.se>
2021-01-23 11:30:32 +0100

    selection: remove duplicate ‘ESC’ in switch case

--- selection.c
+++ selection.c
@@ -1651,10 +1651,6 @@ fdm_receive(struct fdm *fdm, int fd, int events, void *data)
             /* Additional control characters stripped by default (but
              * configurable) in XTerm: BS, HT, DEL */
             case '\b': case '\t': case '\x7f':
-                /* FALLTHROUGH */
-
-            /* ESC */
-            case '\x1b':
                 ctx->decoder(ctx, p, i);
 
                 xassert(i + 1 <= left);
