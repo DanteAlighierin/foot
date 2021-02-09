80cd3df790a16a10e827a355eed37373baf3b72f
Daniel Eklöf <daniel@ekloef.se>
2021-02-09 21:16:40 +0100

    completions: fish: -d,--log-level

--- completions/fish/footclient.fish
+++ completions/fish/footclient.fish
@@ -9,6 +9,7 @@ complete -c footclient -x -s w -l window-size-pixels                            
 complete -c footclient -x -s W -l window-size-chars                                                         -d "window WIDTHxHEIGHT, in characters (not set)"
 complete -c footclient -F -s s -l server-socket                                                             -d "override the default path to the foot server socket ($XDG_RUNTIME_DIR/foot-$WAYLAND_DISPLAY.sock)"
 complete -c footclient    -s H -l hold                                                                      -d "remain open after child process exits"
+complete -c footclient -x -s d -l log-level          -a "info warning error"                                -d "log-level (info)"
 complete -c footclient -x -s l -l log-colorize       -a "always never auto"                                 -d "enable or disable colorization of log output on stderr"
 complete -c footclient    -s v -l version                                                                   -d "show the version number and quit"
 complete -c footclient    -s h -l help                                                                      -d "show help message and quit"
