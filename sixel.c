47e4cfbf5c5f016fd1051ae811e246b3b9e25579
Daniel Eklöf <daniel@ekloef.se>
2021-03-06 19:48:37 +0100

    sixel: ignore invalid sixel characters in DECGRI (repeat)

--- sixel.c
+++ sixel.c
@@ -1049,12 +1049,22 @@ decgri(struct terminal *term, uint8_t c)
         term->sixel.param += c - '0';
         break;
 
-    default:
+    case '?': case '@': case 'A': case 'B': case 'C': case 'D': case 'E':
+    case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L':
+    case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S':
+    case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+    case '[': case '\\': case ']': case '^': case '_': case '`': case 'a':
+    case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h':
+    case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o':
+    case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v':
+    case 'w': case 'x': case 'y': case 'z': case '{': case '|': case '}':
+    case '~': {
         //LOG_DBG("repeating '%c' %u times", c, term->sixel.param);
         for (unsigned i = 0; i < term->sixel.param; i++)
             decsixel(term, c);
         term->sixel.state = SIXEL_DECSIXEL;
         break;
+    }
     }
 }
 
