f3b3bc1f711941096886f752a4272d1990409243
Daniel Eklöf <daniel@ekloef.se>
2021-05-08 19:07:37 +0200

    term: reset: combine memset+initializers of vt struct

--- terminal.c
+++ terminal.c
@@ -1652,9 +1652,10 @@ term_reset(struct terminal *term, bool hard)
     free(term->vt.osc8.uri);
     free(term->vt.osc.data);
 
-    memset(&term->vt, 0, sizeof(term->vt));
-    term->vt.state = 0; /* STATE_GROUND */
-    term->vt.osc8.begin = (struct coord){-1, -1};
+    term->vt = (struct vt){
+        .state = 0,     /* STATE_GROUND */
+        .osc8 = {.begin = (struct coord){-1, -1}},
+    };
 
     if (term->grid == &term->alt) {
         term->grid = &term->normal;
